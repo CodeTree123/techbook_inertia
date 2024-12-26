84e0020f9434c93168723b55d7f2c30966f85bf9
dilshan-ah <dilshaneffendi1123@gmail.com>
2024-12-26 13:01:57 +0600

    google api trial two

--- resources/js/Pages/user/layout/MainLayout.jsx
+++ resources/js/Pages/user/layout/MainLayout.jsx
@@ -1,56 +1,60 @@
 import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import { Head } from '@inertiajs/react';
 
 const MainLayout = ({ children }) => {
-      const [successMessage, setSuccessMessage] = useState('');
-      const [errorMessage, setErrorMessage] = useState('');
-    
-      const handleSuccessMessage = (data) => {
-        setSuccessMessage(data);
-      };
-    
-      const handleErrorMessage = (data) => {
-        setErrorMessage(data);
-      };
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-        useEffect(() => {
-          if (errorMessage) {
-            const timer = setTimeout(() => {
-              setErrorMessage('');
-            }, 1500);
-            return () => clearTimeout(timer);
-          }
-        }, [errorMessage]);
-      
-        useEffect(() => {
-          if (successMessage) {
-            const timer = setTimeout(() => {
-              setSuccessMessage('');
-            }, 1500);
-            return () => clearTimeout(timer);
-          }
-        }, [successMessage]);
-    return (
-        <>
-            <Header onSuccessMessage={handleSuccessMessage} onErrorMessage={handleErrorMessage}/>
-            {children}
-            <Footer />
-            {successMessage && (
-                <div className="alert alert-success alert-dismissible fade show position-fixed" style={{ bottom: '50px', right: '50px', height: 'max-content' }} role="alert">
-                    <span>{successMessage}</span>
-                    <button type="button" className="btn-close" onClick={() => setSuccessMessage(null)} />
-                </div >
-            )}
-            {errorMessage && (
-                <div className="alert alert-danger alert-dismissible fade show position-fixed" style={{ bottom: '50px', right: '50px', height: 'max-content' }} role="alert">
-                    <span>{errorMessage}</span>
-                    <button type="button" className="btn-close" onClick={() => setErrorMessage(null)} />
-                </div >
-            )
-            }
-        </>
-    )
+  const handleSuccessMessage = (data) => {
+    setSuccessMessage(data);
+  };
+
+  const handleErrorMessage = (data) => {
+    setErrorMessage(data);
+  };
+
+  useEffect(() => {
+    if (errorMessage) {
+      const timer = setTimeout(() => {
+        setErrorMessage('');
+      }, 1500);
+      return () => clearTimeout(timer);
+    }
+  }, [errorMessage]);
+
+  useEffect(() => {
+    if (successMessage) {
+      const timer = setTimeout(() => {
+        setSuccessMessage('');
+      }, 1500);
+      return () => clearTimeout(timer);
+    }
+  }, [successMessage]);
+  return (
+    <>
+      <Head>
+        <meta name="csrf-token" content={window.csrfToken} />
+      </Head>
+      <Header onSuccessMessage={handleSuccessMessage} onErrorMessage={handleErrorMessage} />
+      {children}
+      <Footer />
+      {successMessage && (
+        <div className="alert alert-success alert-dismissible fade show position-fixed" style={{ bottom: '50px', right: '50px', height: 'max-content' }} role="alert">
+          <span>{successMessage}</span>
+          <button type="button" className="btn-close" onClick={() => setSuccessMessage(null)} />
+        </div >
+      )}
+      {errorMessage && (
+        <div className="alert alert-danger alert-dismissible fade show position-fixed" style={{ bottom: '50px', right: '50px', height: 'max-content' }} role="alert">
+          <span>{errorMessage}</span>
+          <button type="button" className="btn-close" onClick={() => setErrorMessage(null)} />
+        </div >
+      )
+      }
+    </>
+  )
 }
 
 export default MainLayout
