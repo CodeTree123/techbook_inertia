5d3553ffdb035053f8c7d9345ef0f02eb2ec1229
CodeTree123 <codetree.developers@gmail.com>
2025-01-21 17:47:00 +0600

    sunday 2nd commit

--- resources/js/Pages/user/workOrder/Details/components/Deliverable.jsx
+++ resources/js/Pages/user/workOrder/Details/components/Deliverable.jsx
@@ -1,10 +1,8 @@
 import { router, useForm } from '@inertiajs/react';
-import React from 'react'
+import React from 'react';
 
 const Deliverable = ({ id, details, onSuccessMessage, is_cancelled }) => {
-    const { data, setData, post, delete: deleteItem, errors, processing, recentlySuccessful } = useForm({
-
-    });
+    const { data, setData, post, delete: deleteItem, errors, processing, recentlySuccessful } = useForm({});
 
     const deleteTask = (e, taskId, url) => {
         e.preventDefault();
@@ -13,20 +11,20 @@ const Deliverable = ({ id, details, onSuccessMessage, is_cancelled }) => {
             preserveScroll: true,
             onSuccess: () => {
                 onSuccessMessage('File Deleted Successfully');
-            }
+            },
         });
     };
 
+    // Group tasks and always include a "Misc" group
     const groupedTasks = details?.reduce((acc, task) => {
-        if (task.description) {
-            const normalizedDescription = task.description.toUpperCase();
-            if (!acc[normalizedDescription]) {
-                acc[normalizedDescription] = [];
-            }
-            acc[normalizedDescription].push(task);
+        const description = task.description || 'Misc'; // Default to "Misc" if no description
+        const normalizedDescription = description.toUpperCase();
+        if (!acc[normalizedDescription]) {
+            acc[normalizedDescription] = [];
         }
+        acc[normalizedDescription].push(task);
         return acc;
-    }, {});
+    }, { MISC: [] }); // Ensure "Misc" group always exists
 
     const renderFilePreview = (file, index, taskId) => {
         const fileExtension = file.path.split('.').pop().toLowerCase();
@@ -43,59 +41,75 @@ const Deliverable = ({ id, details, onSuccessMessage, is_cancelled }) => {
                             </a>
                         </div>
                     </div>
-                    <p className='text-danger position-absolute' style={{cursor: 'pointer', right: '5px', top: '0px'}} onClick={(e)=>deleteTask(e, taskId, file.uploaded_at)}>X</p>
+                    <p
+                        className="text-danger position-absolute"
+                        style={{ cursor: 'pointer', right: '5px', top: '0px' }}
+                        onClick={(e) => deleteTask(e, taskId, file.uploaded_at)}
+                    >
+                        X
+                    </p>
                 </div>
-
             );
         } else if (fileExtension === 'pdf') {
             return (
                 <div key={index} className="file-preview position-relative">
                     <div className="file-content">
                         <div>
-                            <i className="fa fa-file-pdf preview-pdf text-danger" aria-hidden="true" style={{ fontSize: '100px' }}></i>
-                            <a href={fileUrl} target="_blank">
+                            <i
+                                className="fa fa-file-pdf preview-pdf text-danger"
+                                aria-hidden="true"
+                                style={{ fontSize: '100px' }}
+                            ></i>
+                            <a href={fileUrl} target="_blank" rel="noopener noreferrer">
                                 <p className="file-name">{file.name}</p>
                             </a>
                         </div>
                     </div>
-                    <p className='text-danger position-absolute' style={{cursor: 'pointer', right: '5px', top: '0px'}} onClick={(e)=>deleteTask(e, taskId, file.uploaded_at)}>X</p>
+                    <p
+                        className="text-danger position-absolute"
+                        style={{ cursor: 'pointer', right: '5px', top: '0px' }}
+                        onClick={(e) => deleteTask(e, taskId, file.uploaded_at)}
+                    >
+                        X
+                    </p>
                 </div>
-
             );
         } else {
             return (
                 <div key={index} className="file-preview position-relative">
                     <div className="file-content">
                         <div>
-                            <i className="fa fa-file preview-pdf" aria-hidden="true" style={{ fontSize: '100px' }}></i>
-                            <a href={fileUrl} target="_blank">
-                                <p className="file-name" style={{cursor: 'pointer', right: '5px', top: '0px'}} onClick={(e)=>deleteTask(e, taskId, file.path)}>{file.uploaded_at}</p>
+                            <i className="fa fa-file preview-generic" aria-hidden="true" style={{ fontSize: '100px' }}></i>
+                            <a href={fileUrl} target="_blank" rel="noopener noreferrer">
+                                <p className="file-name">{file.name}</p>
                             </a>
                         </div>
                     </div>
-                    <p className='text-danger position-absolute'>X</p>
+                    <p
+                        className="text-danger position-absolute"
+                        style={{ cursor: 'pointer', right: '5px', top: '0px' }}
+                        onClick={(e) => deleteTask(e, taskId, file.uploaded_at)}
+                    >
+                        X
+                    </p>
                 </div>
             );
         }
     };
 
     const addFilePhoto = async (e, description) => {
-        // Check if a file was selected
         if (e.target.files && e.target.files.length > 0) {
             const file = e.target.files[0];
-
-            // Create a FormData object
             const formData = new FormData();
             formData.append('file', file);
 
             try {
-                // Send the request using Fetch API or Axios
-                const response = await fetch(route('user.wo.uploadMoreFilePhoto',[id, description]), {
+                const response = await fetch(route('user.wo.uploadMoreFilePhoto', [id, description]), {
                     method: 'POST',
                     headers: {
-                        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content, // Include CSRF token
+                        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content,
                     },
-                    body: formData, // Use FormData as the request body
+                    body: formData,
                 });
 
                 if (response.ok) {
@@ -119,55 +133,59 @@ const Deliverable = ({ id, details, onSuccessMessage, is_cancelled }) => {
     return (
         <div className="card bg-white shadow border-0 mb-4">
             <div className="card-header bg-white d-flex justify-content-between align-items-center">
-                <h3 style={{ fontSize: 20, fontWeight: 600 }}>Deliverables </h3>
+                <h3 style={{ fontSize: 20, fontWeight: 600 }}>Deliverables</h3>
             </div>
             <div className="card-body bg-white">
-                {/* Pre Photo Section */}
-                {Object.keys(groupedTasks).length > 0 ? (
-                    Object.keys(groupedTasks).map((description) => (
-                        <div key={description}>
-                            <h6>{description}</h6>
-                            <div id="preDeliverCont" className="file-preview-container" />
-                            <div className="w-100 pb-2 mb-2 border-bottom">
-                                <div className="d-flex gap-2 flex-wrap">
-                                    {groupedTasks[description].map((task) => {
-                                        if (task.file != null) {
-                                            let files = [];
-                                            try {
-
-                                                // Check if task.file is a JSON string and parse it if necessary
-                                                if (typeof task.file === 'string') {
-                                                    files = Array.isArray(JSON.parse(task.file)) ? JSON.parse(task.file) : [];
-                                                } else if (Array.isArray(task.file)) {
-                                                    files = task.file; // If task.file is already an array, use it directly
-                                                }
-                                            } catch (error) {
-                                                console.error('Error parsing task file:', error);
+                {Object.keys(groupedTasks).map((description) => (
+                    <div key={description}>
+                        <h6>{description}</h6>
+                        <div id="preDeliverCont" className="file-preview-container" />
+                        <div className="w-100 pb-2 mb-2 border-bottom">
+                            <div className="d-flex gap-2 flex-wrap">
+                                {groupedTasks[description].map((task) => {
+                                    if (task.file != null) {
+                                        let files = [];
+                                        try {
+                                            if (typeof task.file === 'string') {
+                                                files = Array.isArray(JSON.parse(task.file)) ? JSON.parse(task.file) : [];
+                                            } else if (Array.isArray(task.file)) {
+                                                files = task.file;
                                             }
-
-                                            return (
-                                                <>
-                                                    {files.map((file, index) => (
-                                                        file && file.path ? renderFilePreview(file, index, task.id ) : null
-                                                    ))}
-                                                </>
-                                            );
+                                        } catch (error) {
+                                            console.error('Error parsing task file:', error);
                                         }
-                                        return null;
-                                    })}
-                                </div>
 
-                                <label htmlFor={`morefile-${description}`} className='py-2' style={{ cursor: 'pointer' }}>+ Add File</label>
-                                <input id={`morefile-${description}`} type="file" className='invisible' onChange={(e) => addFilePhoto(e, description)} disabled={is_cancelled} />
+                                        return (
+                                            <>
+                                                {files.map((file, index) =>
+                                                    file && file.path ? renderFilePreview(file, index, task.id) : null
+                                                )}
+                                            </>
+                                        );
+                                    }
+                                    return null;
+                                })}
                             </div>
+                            <label
+                                htmlFor={`morefile-${description}`}
+                                className="py-2"
+                                style={{ cursor: 'pointer' }}
+                            >
+                                + Add File
+                            </label>
+                            <input
+                                id={`morefile-${description}`}
+                                type="file"
+                                className="invisible"
+                                onChange={(e) => addFilePhoto(e, description)}
+                                disabled={is_cancelled}
+                            />
                         </div>
-                    ))
-                ) : null}
-
+                    </div>
+                ))}
             </div>
         </div>
+    );
+};
 
-    )
-}
-
-export default Deliverable
+export default Deliverable;
